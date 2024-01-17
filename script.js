9da500c5e6c9923a36ec72885958f564788ebe80
abhikainthla <105478999+abhikainthla@users.noreply.github.com>
2024-01-17 15:50:58 +0530

    Added some css

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
     input_text.addEventListener('keyup', () =>{
         let filter = input_text.value;
         console.log(filter);
-        displayResult(filter);
+        displayResult(filter.toLowerCase());
         return false;
     })
     
@@ -23,9 +23,14 @@ let table_info = document.getElementById("table-info");
 table_info.innerHTML ="";
 filterData.forEach(element => {
     let tr = document.createElement("tr");
+    tr.setAttribute('class', 'row');
     let td1 = document.createElement('td');
+    td1.style.fontSize ="32px"
+    td1.setAttribute('class', 'column');
     let td2 = document.createElement('td');
+    td2.setAttribute('class', 'column');
     let td3 = document.createElement('td');
+    td3.setAttribute('class', 'column');
     td1.innerText = element.emoji;
     td2.innerText = element.aliases;
     td3.innerText = element.description;
@@ -39,5 +44,11 @@ form.onsubmit= function (event) {
     event.preventDefault();
 }
 
+function shrinkHeader(){
+    const header = document.querySelector("header");
+    header.style.height ="60px";
+}
+
+window.onscroll = () => shrinkHeader();
 
 window.onload = () => displayResult();
